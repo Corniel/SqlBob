ef3252caa34d47c2b6736d2176318ac773ac82af
Corniel Nobel <mail@corniel.nl>
2022-06-21 17:26:28 +0200

    Decorations.

--- src/SqlBob/Joins/Join_T.cs
+++ src/SqlBob/Joins/Join_T.cs
@@ -1,27 +1,28 @@
-﻿namespace SqlBob
+﻿namespace SqlBob;
+
+/// <summary>Represents a SQL JOIN.</summary>
+/// <typeparam name="T">
+/// The type of the join.
+/// </typeparam>
+public abstract class Join<T> : Join where T: Join<T>
 {
-    /// <summary>Represents a SQL JOIN.</summary>
-    /// <typeparam name="T">
-    /// The type of the join.
-    /// </typeparam>
-    public abstract class Join<T> : Join where T: Join<T>
+    /// <inherritdoc/>
+    protected Join(object expression) 
+        : base(expression) { }
+
+    /// <summary>Sets the AS alias.</summary>
+    [Impure]
+    public T As(Alias alias)
     {
-        /// <inherritdoc/>
-        protected Join(object expression) 
-            : base(expression) { }
+        Alias = alias;
+        return (T)this;
+    }
 
-        /// <summary>Sets the AS alias.</summary>
-        public T As(Alias alias)
-        {
-            Alias = alias;
-            return (T)this;
-        }
-
-        /// <summary>Sets the ON condition</summary>
-        public T On(object condition)
-        {
-            Condition = Convert(condition) ?? SyntaxError.JoinOnConditionNotSpecified;
-            return (T)this;
-        }
+    /// <summary>Sets the ON condition</summary>
+    [Impure]
+    public T On(object condition)
+    {
+        Condition = Convert(condition) ?? SyntaxError.JoinOnConditionNotSpecified;
+        return (T)this;
     }
 }
